494bafe7d07b97f4c9e55b4ab726e338f6c96baa
Kristian Mandrup <kmandrup@gmail.com>
2020-07-11 11:26:32 +0100

    sidebar search

--- packages/sidebar/src/side/entries/SidebarSearch.ts
+++ packages/sidebar/src/side/entries/SidebarSearch.ts
@@ -7,6 +7,15 @@ export class SidebarSearch {
   success: any;
   error: any;
 
+  searchTerms: any;
+  count: number = 20;
+  page: number = 0;
+  results: any[] = [];
+  index: number = 0;
+
+  dict: any = new mxDictionary();
+  tmpDict: any = new mxDictionary();
+
   constructor(sidebar, { success, error }: any = {}) {
     this.sidebar = sidebar;
     this.success = success;
@@ -16,56 +25,111 @@ export class SidebarSearch {
   /**
    * Adds shape search UI.
    */
-  searchEntries(searchTerms, count, page) {
+  searchEntries(searchTerms, { count, page }) {
+    const { onSearchTerms, onNoSearchTerms } = this;
+    onSearchTerms(searchTerms, { count, page }) || onNoSearchTerms();
+  }
+
+  hasSearchTerms(searchTerms) {
+    return this.taglist && searchTerms;
+  }
+
+  set(searchTerms, { count, page }) {
+    this.searchTerms = searchTerms;
+    this.count = count || 20;
+    this.page = page || 0;
+  }
+
+  get terms() {
+    return this.searchTerms.toLowerCase().split(" ");
+  }
+
+  get max() {
+    return (this.page + 1) * this.count;
+  }
+
+  get len() {
+    return this.results.length;
+  }
+
+  onSearchTerms(searchTerms, { count, page }) {
+    this.set(searchTerms, { count, page });
+    const { hasSearchTerms, success } = this;
+    if (!hasSearchTerms(searchTerms)) return;
+
+    const { resetResults, terms, processSearchTerm } = this;
+    resetResults();
+
+    terms.map((term) => {
+      processSearchTerm(term);
+    });
+
+    const { results, len } = this;
+    success(results.slice(page * count, (page + 1) * count), len, false, terms);
+    return true;
+  }
+
+  resetResults() {
+    this.results = [];
+    this.index = 0;
+    this.tmpDict = new mxDictionary();
+    this.dict = new mxDictionary();
+  }
+
+  processSearchTerm(term) {
+    const { processEntry } = this;
+    if (term.length === 0) return;
+    const entry = this.taglist[term];
+    const tmpDict = new mxDictionary();
+    processEntry(entry);
+    this.dict = tmpDict;
+    this.index++;
+  }
+
+  processEntry(entry) {
+    const { processSearchResults } = this;
+    if (!entry) {
+      this.results = [];
+      return;
+    }
+    processSearchResults(entry);
+  }
+
+  processSearchResults(entry) {
+    const searchResults = entry.entries;
+    this.results = [];
+    const {
+      index,
+      dict,
+      tmpDict,
+      results,
+      page,
+      count,
+      max,
+      terms,
+      success,
+    } = this;
+
+    searchResults.find((searchResult, i) => {
+      // NOTE Array does not contain duplicates
+      if ((index == 0) == (dict.get(searchResult) == null)) {
+        tmpDict.put(searchResult, searchResult);
+        results.push(searchResult);
+
+        if (i == terms.length - 1 && results.length == max) {
+          success(results.slice(page * count, max), max, true, terms);
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+
+  onNoSearchTerms() {
     const {
       success,
       // error
     } = this;
-
-    if (this.taglist != null && searchTerms != null) {
-      var tmp = searchTerms.toLowerCase().split(" ");
-      var dict = new mxDictionary();
-      var max = (page + 1) * count;
-      var results: any[] = [];
-      var index = 0;
-
-      for (var i = 0; i < tmp.length; i++) {
-        if (tmp[i].length > 0) {
-          var entry = this.taglist[tmp[i]];
-          var tmpDict = new mxDictionary();
-
-          if (entry != null) {
-            var arr = entry.entries;
-            results = [];
-
-            for (var j = 0; j < arr.length; j++) {
-              var entry = arr[j];
-
-              // NOTE Array does not contain duplicates
-              if ((index == 0) == (dict.get(entry) == null)) {
-                tmpDict.put(entry, entry);
-                results.push(entry);
-
-                if (i == tmp.length - 1 && results.length == max) {
-                  success(results.slice(page * count, max), max, true, tmp);
-
-                  return;
-                }
-              }
-            }
-          } else {
-            results = [];
-          }
-
-          dict = tmpDict;
-          index++;
-        }
-      }
-
-      var len = results.length;
-      success(results.slice(page * count, (page + 1) * count), len, false, tmp);
-    } else {
-      success([], null, null, tmp);
-    }
+    success([], null, null, []);
   }
 }
--- packages/sidebar/src/side/entries/index.ts
+++ packages/sidebar/src/side/entries/index.ts
@@ -1,1 +1,2 @@
 export * from "./SidebarEntries";
+export * from "./SidebarSearch";
